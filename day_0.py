eb26fdc3e82ce7f15bf8a94fec25b9b14193d88c
flavia <flavia.alvarez@pedidosya.com>
2020-09-30 16:07:06 -0300

    tasks's description

--- day_0.py
+++ day_0.py
@@ -1,5 +1,7 @@
 # %%
-# Calculate the mean, median and mode
+#Task1. Given an array, X, of N integers, calculate and print the respective mean, median, and mode
+# on separate lines. If your array contains more than one modal value, choose the numerically smallest one.
+
 def custom_mean (numbers):
     return sum(numbers)/ len(numbers)
 
@@ -43,7 +45,9 @@ print(mean, median, mode
 
 
 # %%
-# Calculate the weighted mean
+# Task2. Given an array, X, of N integers and an array, W, representing the respective weights
+# of X's elements, calculate and print the weighted mean of X's elements.
+# Your answer should be rounded to a scale of 1 decimal place (i.e.,12.3 format).
 x = 5
 numbers = [10, 40, 30, 50, 20]
 weights = [1, 2, 3, 4, 5]
--- day_1.py
+++ day_1.py
@@ -1,4 +1,7 @@
-# Calculate quartiles
+#%%
+#Task1. Given an array, X, of n integers, calculate the respective first quartile (Q1)
+#, second quartile (Q2), and third quartile (Q3). It is guaranteed that Q1, Q2, and Q3 are integers.
+
 def custom_median(numbers) :
 
     numbers = sorted(numbers)
@@ -28,36 +31,14 @@ numbers = [3, 7, 8, 5, 12, 14, 21, 13, 18]
 for n in custom_quartiles(numbers):
     print(n)
 
+#%%
+#Task2. The interquartile range of an array is the difference between its first (Q1) and third (Q3) quartiles.
+# Given an array, X, of n integers and an array, F, representing the respective frequencies of X's elements,
+# construct a data set, S, where each xi occurs at frequency fi.
+# Then calculate and print S's interquartile range, rounded to a scale of 1 decimal place.
 
-# Calculate Standard Deviation
+Tip: Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, and be sure to not include the median in your upper and lower data sets.
 
-def custom_median(numbers) :
-        
-    numbers = sorted(numbers)
-    middle = len(numbers)//2
-
-    if len(numbers)%2 == 0:
-        return (numbers[middle-1]+numbers[middle])/2 
-    else:
-        return numbers[middle]
-
-def custom_mean (numbers):
-    return sum(numbers)/ len(numbers)
-
-def custom_std_dev(numbers):
-
-    mean = custom_mean(numbers)
-    numerator = sum( (x - mean) ** 2 for x in numbers)
-    denominator = len(numbers)
-    return (numerator/denominator)**(1/2)
-
-x = 5
-numbers = [10, 40, 30, 50, 20]
-
-print(custom_std_dev(numbers))
-
-
-#Calculate interquantile
 def create_repnumbers(numbers, reps):
     lst = []
     for n, r in zip(numbers, reps):
@@ -85,3 +66,33 @@ reps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10
 
 rep_numbers = create_repnumbers(numbers, reps)
 custom_interquartiles(rep_numbers)
+
+#%%
+#Task3. Given an array, X, of N integers, calculate and print the standard deviation.
+# Your answer should be in decimal form, rounded to a scale of 1 decimal place (i.e., 12.3 format).
+# An error margin of +/-0.1 will be tolerated for the standard deviation.
+
+def custom_median(numbers) :
+        
+    numbers = sorted(numbers)
+    middle = len(numbers)//2
+
+    if len(numbers)%2 == 0:
+        return (numbers[middle-1]+numbers[middle])/2 
+    else:
+        return numbers[middle]
+
+def custom_mean (numbers):
+    return sum(numbers)/ len(numbers)
+
+def custom_std_dev(numbers):
+
+    mean = custom_mean(numbers)
+    numerator = sum( (x - mean) ** 2 for x in numbers)
+    denominator = len(numbers)
+    return (numerator/denominator)**(1/2)
+
+x = 5
+numbers = [10, 40, 30, 50, 20]
+
+print(custom_std_dev(numbers))
